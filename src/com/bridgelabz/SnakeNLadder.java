c8856d5a7aaa06a99a3525a45ec2936d8025586e
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-08 21:16:45 +0530

    Added: Final Win Report

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -4,13 +4,52 @@ public class SnakeNLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int N = 100;
-		int moves[] = new int[N];
-		for (int i = 0; i < N; i++)
-			moves[i] = -1;
+		System.out.println("snake and ladder");
 		
-		double ROLL_DICE = Math.floor(Math.random()*10)%7;
-		System.out.println("Dice Roll : "+ROLL_DICE);
+		int currentPosition=0,position=0,ladder=1,counter=0,win=0;
+		int snakes[]= {6,11,19,23,45,66,98};
+		int noPlay[]= {9,15,27,33,51,67,89};
+		
+		while(currentPosition<101) {
+			int dice=(int)Math.floor(Math.random()*10)%6+1;
+			position=dice+currentPosition;
+			System.out.println("dice roll "+dice);
+			for (int element : snakes) {
+	            if (element == position) {
+	                currentPosition= currentPosition-dice;
+	                ladder=0;
+	            }
+	        }
+			for (int element : noPlay) {
+	            if (element == position) {
+	            	currentPosition= currentPosition;
+	                ladder=0;
+	            }
+	        }
+			if (ladder == 1) {
+				currentPosition+=dice;
+			}
+			ladder=1;
+			
+			counter++;
+			
+			if(currentPosition <0) {
+				currentPosition=0;
+			}
+			
+			if(currentPosition == 100) {
+				System.out.println("WINNER");
+				break;
+			}
+			
+			if(currentPosition >100)
+			{
+			currentPosition=win;	
+			}
+			win=currentPosition;
+			position= currentPosition;
+			System.out.println("Current Position "+currentPosition);
+			}
+		System.out.println("total rolls "+counter);
 	}
-
 }
