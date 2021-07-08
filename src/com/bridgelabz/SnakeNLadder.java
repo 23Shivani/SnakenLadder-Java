b323b6c9925a9e99ec28842a7385e67a9c648af0
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-08 20:01:28 +0530

    Added: Start with Position Zero

---
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class SnakeNLadder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = 100;
+		int moves[] = new int[N];
+		for (int i = 0; i < N; i++)
+			moves[i] = -1;
+	}
+
+}
