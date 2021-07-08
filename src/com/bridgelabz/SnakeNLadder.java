054130f12bf9e0f715ab4113505923902081fe34
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-08 20:59:56 +0530

    Added: Dice Roll Randomly

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -8,6 +8,9 @@ public class SnakeNLadder {
 		int moves[] = new int[N];
 		for (int i = 0; i < N; i++)
 			moves[i] = -1;
+		
+		double ROLL_DICE = Math.floor(Math.random()*10)%7;
+		System.out.println("Dice Roll : "+ROLL_DICE);
 	}
 
 }
